318f763d7fac5c57798123e92628f16a6c82f7b6
nbrennan26 <nbrennan26@gmail.com>
2022-05-09 11:53:32 -0500

    Updating tic tac toe logic/style

--- src/apps/tictactoe/TicTacToe.jsx
+++ src/apps/tictactoe/TicTacToe.jsx
@@ -13,6 +13,7 @@ function TicTacToe() {
     name: "Player Two",
     human: false,
   });
+  const [selectedSquare, setSelectedSquare] = useState("");
   const [gameBoard, setGameBoard] = useState({
     square0: null,
     square1: null,
@@ -25,6 +26,44 @@ function TicTacToe() {
     square8: null,
   });
 
+  const claimSquare = (e) => {
+    let tgtSquare = "square" + e.target.classList[1].slice(7);
+    setSelectedSquare(tgtSquare);
+  };
+
+  useEffect(() => {
+    if (selectedSquare !== "" && gameBoard[selectedSquare] == null) {
+      setGameBoard({
+        ...gameBoard,
+        [selectedSquare]: playerTurn,
+      });
+      stateCheck();
+    }
+  }, [selectedSquare]);
+
+  useEffect(() => {
+    if (selectedSquare !== "") {
+      advanceGame();
+      stateCheck();
+    }
+  }, [gameBoard]);
+
+  const stateCheck = () => {
+    console.log("Player Turn", playerTurn);
+    console.log("Turn Count", turnCount);
+    console.log("Selected Square", selectedSquare);
+    console.log("Game Board", gameBoard);
+  };
+
+  const advanceGame = () => {
+    setTurnCount(turnCount + 1);
+    if (playerTurn === "player1") {
+      setPlayerTurn("player2");
+    } else {
+      setPlayerTurn("player1");
+    }
+  };
+
   return (
     <>
       <div className="game-title-cont">
@@ -35,16 +74,157 @@ function TicTacToe() {
         <div className="ttt-scripts"></div>
         <div className="scorebaord"></div>
       </div>
-      <div className={playerTurn === "player1" ? ("ttt-board-cont p1-board") : ("ttt-board-cont p2-board")}>
-        <div className="ttt-square square-zero"></div>
-        <div className="ttt-square square-one"></div>
-        <div className="ttt-square square-two"></div>
-        <div className="ttt-square square-three"></div>
-        <div className="ttt-square square-four"></div>
-        <div className="ttt-square square-five"></div>
-        <div className="ttt-square square-six"></div>
-        <div className="ttt-square square-seven"></div>
-        <div className="ttt-square square-eight"></div>
+      <div
+        className={
+          playerTurn === "player1"
+            ? "ttt-board-cont p1-board"
+            : "ttt-board-cont p2-board"
+        }
+      >
+        <div
+          className={
+            gameBoard.square0 === "player2"
+              ? "ttt-square square-0 p2-square"
+              : gameBoard.square0 === "player1"
+              ? "ttt-square square-0 p1-square"
+              : "ttt-square square-0 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square0 === "player2"
+            ? "O"
+            : gameBoard.square0 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square1 === "player2"
+              ? "ttt-square square-1 p2-square"
+              : gameBoard.square1 === "player1"
+              ? "ttt-square square-1 p1-square"
+              : "ttt-square square-1 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square1 === "player2"
+            ? "O"
+            : gameBoard.square1 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square2 === "player2"
+              ? "ttt-square square-2 p2-square"
+              : gameBoard.square2 === "player1"
+              ? "ttt-square square-2 p1-square"
+              : "ttt-square square-2 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square2 === "player2"
+            ? "O"
+            : gameBoard.square2 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square3 === "player2"
+              ? "ttt-square square-3 p2-square"
+              : gameBoard.square3 === "player1"
+              ? "ttt-square square-3 p1-square"
+              : "ttt-square square-3 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square3 === "player2"
+            ? "O"
+            : gameBoard.square3 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square4 === "player2"
+              ? "ttt-square square-4 p2-square"
+              : gameBoard.square4 === "player1"
+              ? "ttt-square square-4 p1-square"
+              : "ttt-square square-4 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square4 === "player2"
+            ? "O"
+            : gameBoard.square4 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square5 === "player2"
+              ? "ttt-square square-5 p2-square"
+              : gameBoard.square5 === "player1"
+              ? "ttt-square square-5 p1-square"
+              : "ttt-square square-5 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square5 === "player2"
+            ? "O"
+            : gameBoard.square5 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square6 === "player2"
+              ? "ttt-square square-6 p2-square"
+              : gameBoard.square6 === "player1"
+              ? "ttt-square square-6 p1-square"
+              : "ttt-square square-6 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square6 === "player2"
+            ? "O"
+            : gameBoard.square6 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square7 === "player2"
+              ? "ttt-square square-7 p2-square"
+              : gameBoard.square7 === "player1"
+              ? "ttt-square square-7 p1-square"
+              : "ttt-square square-7 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square7 === "player2"
+            ? "O"
+            : gameBoard.square7 === "player1"
+            ? "X"
+            : ""}
+        </div>
+        <div
+          className={
+            gameBoard.square8 === "player2"
+              ? "ttt-square square-8 p2-square"
+              : gameBoard.square8 === "player1"
+              ? "ttt-square square-8 p1-square"
+              : "ttt-square square-8 neutral-square"
+          }
+          onClick={claimSquare}
+        >
+          {gameBoard.square8 === "player2"
+            ? "O"
+            : gameBoard.square8 === "player1"
+            ? "X"
+            : ""}
+        </div>
       </div>
     </>
   );
